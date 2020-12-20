34a703ce1252019bd2b1cbd1ab6c22d46cfee07f
Denis A. Shishkin <denis.outsidepro@gmail.com>
2020-12-21 01:05:44 +0300

    Removed the category switch from.

--- Set Properties Ribbon to track, master track, item or envelope properties depending on focus and report them.lua
+++ Set Properties Ribbon to track, master track, item or envelope properties depending on focus and report them.lua
@@ -8,7 +8,6 @@ License: MIT License
 reaper.Undo_BeginBlock()
 package.path = ({reaper.get_action_context()})[2]:match('^.+[\\//]')..'engine\\' .. "?.lua"
 require "properties_ribbon"
-lastLayout = extstate.get("currentLayout")
 context = reaper.GetCursorContext()
 if context == 0 then
 if reaper.IsTrackSelected(reaper.GetMasterTrack()) then
@@ -21,19 +20,11 @@ extState = "item_properties"
 elseif context == 2 then
 extState = "envelope_properties"
 end
-if lastLayout == extState then
-shouldSwitch = true
-else
 extstate.set("currentLayout", extState)
 extstate.set("speakLayout", tostring(true))
-end
+
 if script_init() then
-local message = initOutputMessage()
-if shouldSwitch == true then
-message(script_switchSublayout())
-end
-message(script_reportOrGotoProperty())
-reaper.osara_outputMessage(tostring(message))
+reaper.osara_outputMessage(script_reportOrGotoProperty())
 script_finish()
 end
 
