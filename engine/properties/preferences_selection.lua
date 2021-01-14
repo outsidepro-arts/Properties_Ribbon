4443281001cb274a4833783c4462036ba38f5d5b
Denis A. Shishkin <denis.outsidepro@gmail.com>
2021-01-14 05:03:17 +0300

    +Added the REAPER preferences layout.
    Now you can set a preferences key once and call all native preferences and also of some extensions whichinstalled by pressing one shortcut and use standart Properties Ribbon logic!
    
    The layout supports ReaPack and SWS extensions preferences by respectived sub-layouts. If one of isn't installed, its sub-layout will not showed.

---
+++ engine/properties/preferences_selection.lua
@@ -0,0 +1,103 @@
+--[[
+This file is part of script complex Properties Ribbon
+Copyright (c) 2020-2021 outsidepro-arts
+License: MIT License
+
+----------
+
+Let me say a few word before starts
+LUA - is not object oriented programming language, but very flexible. Its flexibility allows to realize OOP easy via metatables. In this scripts the pseudo OOP has been used, therefore we have to understand some terms.
+.1 When i'm speaking "Class" i mean the metatable variable with some fields.
+2. When i'm speaking "Method" i mean a function attached to a field or submetatable field.
+When i was starting write this scripts complex i imagined this as real OOP. But in consequence the scripts structure has been reunderstanded as current structure. It has been turned out more comfort as for writing new properties table, as for call this from main script engine.
+After this preambula, let me begin.
+]]--
+
+-- It's just another vision of Properties Ribbon can be applied on
+
+-- Reading the sublayout
+sublayout = extstate.get(currentLayout.."_sublayout")
+if sublayout == "" or sublayout == nil then
+sublayout = "reaperPrefs"
+end
+
+parentLayout = initLayout("%spreferences")
+
+function parentLayout.canProvide()
+return true
+end
+
+parentLayout:registerSublayout("reaperPrefs", "REAPER ")
+parentLayout:registerSublayout("extPrefs", "Extensions ")
+if reaper.APIExists("ReaPack_AboutInstalledPackage") == true then
+parentLayout:registerSublayout("reaPackLayout", "ReaPack extension ")
+end
+if reaper.APIExists("CF_GetSWSVersion") == true then
+parentLayout:registerSublayout("swsLayout", "SWS extension ")
+end
+
+
+
+-- The properties functions template
+
+local function getUsualProperty(_cmd, _msg)
+local usual = {
+-- the Main_OnCommand ID
+cmd = _cmd,
+-- The property label
+msg = _msg,
+get = function(self)
+local message = initOutputMessage()
+message:initType(config.getinteger("typeLevel", 1), string.format("Perform this property to open the %s dialog.", self.msg), "Performable")
+message(self.msg)
+return message
+end,
+set = function(self, action)
+if action == nil then
+reaper.Main_OnCommand(self.cmd, 1)
+return ""
+else
+return "This property is performable only."
+end
+end
+}
+return usual
+end
+
+
+-- REAPER preferences
+parentLayout.reaperPrefs:registerProperty(getUsualProperty(40016, "REAPER preferences"))
+
+-- Metronome/Pre-roll setings
+parentLayout.reaperPrefs:registerProperty(getUsualProperty(40363, "Show metronome or pre-roll settings"))
+
+-- Snap/Grid settings
+parentLayout.reaperPrefs:registerProperty(getUsualProperty(40071, "Show snap or grid settings"))
+
+-- External time synchronization settings
+parentLayout.reaperPrefs:registerProperty(getUsualProperty(40619, "Show external timecode synchronization settings"))
+
+-- OSARA configuration
+-- We will not check OSARA install status,cuz it's supposet should be installed there
+parentLayout.extPrefs:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_OSARA_CONFIG"), "OSARA configuration"))
+
+-- ReaPack actions
+if parentLayout.reaPackLayout then
+parentLayout.reaPackLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_REAPACK_BROWSE"), "Browse packages"))
+parentLayout.reaPackLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_REAPACK_IMPORT"), "Import repositories"))
+parentLayout.reaPackLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_REAPACK_MANAGE"), "Manage repositories"))
+parentLayout.reaPackLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_REAPACK_SYNC"), "Synchronize packages"))
+parentLayout.reaPackLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_REAPACK_UPLOAD"), "Upload packages"))
+parentLayout.reaPackLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_REAPACK_ABOUT"), "About currently installed ReaPack"))
+end
+
+-- SWS actions
+if parentLayout.swsLayout then
+parentLayout.swsLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_BR_LOUDNESS_PREF"), "Global loudness preferences"))
+parentLayout.swsLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_XENAKIOS_DISKSPACECALC"), "Disk space calculator"))
+parentLayout.swsLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_AUTORENDER_PREFERENCES"), "Autorender: Global Preferences"))
+parentLayout.swsLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_SWS_ABOUT"), "About currently installed SWS extension"))
+parentLayout.swsLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_BR_VERSION_CHECK"), "Check for new SWS version"))
+end
+
+return parentLayout[sublayout]
