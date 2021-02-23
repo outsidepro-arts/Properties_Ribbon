dbe36b4bc419df6bcac799a258f952d20a7ce494
Denis A. Shishkin <denis.outsidepro@gmail.com>
2021-02-23 08:08:52 +0300

    *Reworked the property:
    *1. Now property report the bypass state.
    *2. The type prompts now more concreted.
    +3. Monitoring section actions support..
    Please note: in monitoring section category you will see only two actions, but FX parameters will apsent.. Yeah no any actions for monitoring FX list here, cuz OSARA proposes their list in master track fx parameters.

--- engine/properties/fx_actions.lua
+++ engine/properties/fx_actions.lua
@@ -15,6 +15,8 @@ After this preambula, let me begin.
 
 -- It's just another vision of Properties Ribbon can be applied on
 
+local sublayout = extstate[currentLayout.."_sublayout"] or "mainLayout"
+
 local contexts = {
 [0]="Current track",
 [1]="Selected item take",
@@ -31,13 +33,22 @@ end
 
 -- Checking availableness of context perform
 local function isAvailable()
-if (context == 0 and reaper.TrackFX_GetCount(reaper.GetLastTouchedTrack()) > 0) or (context == 1 and reaper.TakeFX_GetCount(reaper.GetActiveTake(reaper.GetSelectedMediaItem(0, 0))) > 0) or (context == true and reaper.TrackFX_GetCount(reaper.GetMasterTrack()) > 0 or reaper.TrackFX_GetRecCount(reaper.GetMasterTrack()) > 0) then
+if (context == 0 and reaper.TrackFX_GetCount(reaper.GetLastTouchedTrack()) > 0) or (context == 1 and reaper.TakeFX_GetCount(reaper.GetActiveTake(reaper.GetSelectedMediaItem(0, 0))) > 0) or (context == true and reaper.TrackFX_GetCount(reaper.GetMasterTrack()) > 0) then
 return true
 end
 return false
 end
 
-local fxActionsLayout = initLayout(("%s FX actions"):format(contexts[context]))
+local fxActionsLayout = nil
+-- We should check the monitoring section FX count to solve create a sublayouts or not
+if reaper.TrackFX_GetRecCount(reaper.GetMasterTrack()) == 0 then
+fxActionsLayout = initLayout(("%s FX actions"):format(contexts[context]))
+else
+fxActionsLayout = initLayout("%sFX actions")
+fxActionsLayout:registerSublayout("mainLayout", contexts[context])
+fxActionsLayout:registerSublayout("monitoringLayout", "Monitoring")
+end
+
 
 function fxActionsLayout.canProvide()
 local context = reaper.GetCursorContext()
@@ -47,27 +58,68 @@ end
 return false
 end
 
+-- Registering properties macros
+-- We need this because it forces us check a sublayout existing each time
+local function registerProperty(sl, property)
+if fxActionsLayout[sl] then
+fxActionsLayout[sl]:registerProperty(property)
+else
+fxActionsLayout:registerProperty(property)
+end
+end
 
-local function getUsualProperty(_cmd, _msg, availableness)
-local usual = {
--- the Main_OnCommand ID
-cmd = _cmd,
--- The property label
-msg = _msg,
+-- FX chain action
+registerProperty("mainLayout", {
 get = function(self)
 local message = initOutputMessage()
-message:initType(config.getinteger("typeLevel", 1), string.format("Perform this property to call the %s action.", self.msg), "Performable")
-if not availableness then
+message:initType(config.getinteger("typeLevel", 1), ("Perform this property to show the %s FX chain."):format(contexts[context]), "Performable")
+message(("View FX chain for %s"):format(contexts[context]))
+return message
+end,
+set = function(self, action)
+local commands = {
+[0]=40291,
+[1]=40638,
+[true]=40846
+}
+if action == nil then
+reaper.Main_OnCommand(commands[context], 1)
+return ""
+else
+return "This property is performable only."
+end
+end
+})
+
+-- Bypass all FX action
+-- It must not be created if context is items and SWS is not installed
+if context ~= 1 and not reaper.APIExists("CF_GetSWSVersion") == false then
+registerProperty("mainLayout", {
+states = {
+[0]="Activate",
+[1]="Bypass"
+},
+commands = {
+[0]=40298,
+[1]=reaper.NamedCommandLookup("_S&M_TGL_TAKEFX_BYP"),
+[true]=16
+},
+get = function(self)
+local message = initOutputMessage()
+message:initType(config.getinteger("typeLevel", 1), ("Toggle this property to bypass or activate the %s FX chain."):format(contexts[context]), "Toggleable")
+if not isAvailable() then
 message:addType(" This action is unavailable right now because there are no FX.", 1)
 message:changeType("Unavailable", 2)
 end
-message(self.msg)
+local state = reaper.GetMediaTrackInfo_Value(reaper.GetLastTouchedTrack(), "I_FXEN")
+message(("%s all %s FX"):format(self.states[state], contexts[context]))
 return message
 end,
 set = function(self, action)
 if action == nil then
-if availableness then
-reaper.Main_OnCommand(self.cmd, 1)
+if isAvailable() then
+local state = reaper.GetMediaTrackInfo_Value(reaper.GetLastTouchedTrack(), "I_FXEN")
+reaper.Main_OnCommand(self.commands[context], nor(state))
 return ""
 else
 return "This action is unavailable right now because no one FX is set there."
@@ -76,22 +128,90 @@ else
 return "This property is performable only."
 end
 end
-}
-return usual
+})
 end
 
-if context == 0 then
-fxActionsLayout:registerProperty(getUsualProperty(40291, "View FX chain for current track", true))
-fxActionsLayout:registerProperty(getUsualProperty(40298, "Toggle FX bypass for  current track", isAvailable()))
-elseif context == 1 then
-fxActionsLayout:registerProperty(getUsualProperty(40638, "Show FX chain for item take", true))
-if reaper.APIExists("CF_GetSWSVersion") == true then
-fxActionsLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_S&M_TGL_TAKEFX_BYP"), "Toggle all take FX bypass for selected items", isAvailable()))
+-- OSARA FX parameters action
+registerProperty("mainLayout", {
+get = function(self)
+local message = initOutputMessage()
+message:initType(config.getinteger("typeLevel", 1), ("Perform this property to show the %s FX parameters using OSARA."):format(contexts[context]), "Performable")
+if context == true and reaper.TrackFX_GetRecCount(reaper.GetMasterTrack()) > 0 then
+message:addType(" The FX which have being added to monitoring section will display also here.", 1)
 end
-elseif context == true then
-fxActionsLayout:registerProperty(getUsualProperty(40846, "View FX chain for master track", true))
-fxActionsLayout:registerProperty(getUsualProperty(16, "Toggle FX bypass for master track", isAvailable()))
+local available = (isAvailable() == true or reaper.TrackFX_GetRecCount(reaper.GetMasterTrack()) > 0)
+if not available then
+message:addType(" This action is unavailable right now because there are no FX.", 1)
+message:changeType("Unavailable", 2)
 end
-fxActionsLayout:registerProperty(getUsualProperty(reaper.NamedCommandLookup("_OSARA_FXPARAMS"), ("View OSARA FX parameters for %s"):format(contexts[context]), isAvailable()))
+message(("View OSARA FX parameters for %s"):format(contexts[context]))
+if context == true and reaper.TrackFX_GetRecCount(reaper.GetMasterTrack()) > 0 then
+message(" and monitoring section")
+end
+return message
+end,
+set = function(self, action)
+if action == nil then
+local available = (isAvailable() == true or reaper.TrackFX_GetRecCount(reaper.GetMasterTrack()) > 0)
+if available then
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_OSARA_FXPARAMS"), 0)
+return ""
+else
+return "This action is unavailable right now because no one FX is set there."
+end
+else
+return "This property is performable only."
+end
+end
+})
+
+-- Monitoring FX actions
+-- To these action will be available, an user should activate the monitoring FX chain from the appropriated menu and set some there, so anyone shouldn't see the superfluous sublayouts.
+ if reaper.TrackFX_GetRecCount(reaper.GetMasterTrack()) > 0 then
+-- Monitoring FX chain
+registerProperty("monitoringLayout", {
+get = function(self)
+local message = initOutputMessage()
+message:initType(config.getinteger("typeLevel", 1), "Perform this property to show the monitoring FX chain.", "Performable")
+message("View the monitoring FX chain")
+return message
+end,
+set = function(self, action)
+if action == nil then
+reaper.Main_OnCommand(41882, 0)
+return ""
+else
+return "This property is performable only."
+end
+end
+})
+
+-- Monitoring bypass
+registerProperty("monitoringLayout", {
+states = {
+[0]="Bypass",
+[1]="Activate"
+},
+get = function(self)
+local message = initOutputMessage()
+message:initType(config.getinteger("typeLevel", 1), "Toggle this property to bypass or activate all monitoring FX.", "Toggleable")
+local state = reaper.GetToggleCommandState(41884)
+message(("%s all monitoring FX"):format(self.states[state]))
+return message
+end,
+set = function(self, action)
+if action == nil then
+local state = reaper.GetToggleCommandState(41884)
+reaper.Main_OnCommand(41884, nor(state))
+return ""
+else
+return "This property is performable only."
+end
+end
+})
+
+return fxActionsLayout[sublayout]
+end
+
 
 return fxActionsLayout
