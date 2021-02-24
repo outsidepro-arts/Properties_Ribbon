135ffe2f00793a6cd7595dbcae035b7c9c3ea2b5
Denis A. Shishkin <denis.outsidepro@gmail.com>
2021-02-24 06:05:16 +0300

    *Used newly added function instead of performing its code

--- Set Properties Ribbon to track, master track, item or envelope properties depending on focus and report them.lua
+++ Set Properties Ribbon to track, master track, item or envelope properties depending on focus and report them.lua
@@ -9,20 +9,7 @@ reaper.Undo_BeginBlock()
 package.path = ({reaper.get_action_context()})[2]:match('^.+[\\//]')..'engine\\' .. "?.lua"
 require "properties_ribbon"
 
-context = reaper.GetCursorContext()
-if context == 0 then
-if reaper.IsTrackSelected(reaper.GetMasterTrack()) then
-contextLayout = "mastertrack_properties"
-else
-contextLayout = "track_properties"
-end
-elseif context == 1 then
-contextLayout = "item_properties"
-elseif context == 2 then
-contextLayout = "envelope_properties"
-end
-
-if script_init(contextLayout) then
+if script_init(proposeLayout(true)) then
 reaper.osara_outputMessage(script_reportOrGotoProperty())
 script_finish()
 end
