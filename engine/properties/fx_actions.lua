57c57d93e8ce903ac9dc56db9c2776ed4132131d
Denis A. Shishkin <denis.outsidepro@gmail.com>
2021-05-30 00:54:58 +0300

    *Fixed the ReaScript error when you're trying to call the layout on unsupported context.

--- engine/properties/fx_actions.lua
+++ engine/properties/fx_actions.lua
@@ -20,6 +20,7 @@ local sublayout = extstate[currentLayout.."_sublayout"] or "contextLayout"
 local contexts = {
 [0]="Current track",
 [1]="Selected item take",
+[2] = "Envelopes"
 }
 
 local function getStringPluginsCount(where)
