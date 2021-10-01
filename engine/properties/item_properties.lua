outsidepro-arts/Properties_Ribbon

commit 4cbc1cc94e6e152e27ae1a9e8b62ef7debe9f165

*Fixed ReaScript error when user had tries to select a few items and get its properties.

engine/properties/item_properties.lua
@@ -102,9 +102,9 @@ local state = func(items[k])
 local prevState if items[k-1] then prevState = func(items[k-1]) end
 local nextState if items[k+1] then nextState = func(items[k+1]) end
 if state ~= prevState and state == nextState then
-message(string.format("items from %u ", getItemID(items[k])))
+message(string.format("items from %s ", getItemID(items[k])))
 elseif state == prevState and state ~= nextState then
-message(string.format("to %u ", getItemID(items[k])))
+message(string.format("to %s ", getItemID(items[k])))
 if inaccuracy and type(state) == "number" then
 message(string.format("%s", states[state+inaccuracy]))
 else

commit 6730616b0b87c0df2f75f19e85c40b2de8510ca3

*Keep fixing the bugs started from 4cbc1cc94e6e152e27ae1a9e8b62ef7debe9f165
I hope I finalized this.

engine/properties/item_properties.lua
@@ -136,9 +136,9 @@ local state, takeIDX = func(items[k]), getTakeNumber(items[k])
 local prevState, prevTakeIDX if items[k-1] then prevState, prevTakeIDX = func(items[k-1]), getTakeNumber(items[k-1]) end
 local nextState, nextTakeIDX if items[k+1] then nextState, nextTakeIDX = func(items[k+1]), getTakeNumber(items[k+1]) end
 if (state ~= prevState and state == nextState) and (takeIDX ~= prevTakeIDX and takeIDX == nextTakeIDX) then
-message(string.format("%s of items from %u ", getTakeID(items[k]), getItemID(items[k])))
+message(string.format("%s of items from %s ", getTakeID(items[k]), getItemID(items[k])))
 elseif (state == prevState and state ~= nextState) and (takeIDX == prevTakeIDX and takeIDX ~= nextTakeIDX) then
-message(string.format("to %s of item %u", getTakeID(items[k]), getItemID(items[k])))
+message(string.format("to %s of item %s", getTakeID(items[k]), getItemID(items[k])))
 if inaccuracy and type(state) == "number" then
 message(string.format("%s", states[state+inaccuracy]))
 else