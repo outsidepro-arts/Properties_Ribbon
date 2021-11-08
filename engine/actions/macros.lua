02c0d6dcfc4f7aa5bf434686cf26702c7789219e
Denis A. Shishkin <denis.outsidepro@gmail.com>
2021-11-08 19:46:37 +0300

    *Commands list support and extended for the new actions system.
    Now you can pass the simple array with commands instead of command number to cmd. In this case set method will perform these in turn. This is analogue for custom actions in REAPER.

--- engine/actions/macros.lua
+++ engine/actions/macros.lua
@@ -11,7 +11,7 @@ License: MIT License
 -- You don't need to include this file. The engine will do it itself.
 
 function getUsualProperty(
--- the Main_OnCommand ID
+-- the Main_OnCommand ID or its list
 cmd,
 -- The property label
 msg
@@ -19,9 +19,11 @@ msg
 local usual = {
 get = function(self)
 -- If user has SWS installed, omit the msg parameter
+if type(cmd) ~= "table" then
 if reaper.APIExists("CF_GetCommandText") then
 msg = string.match(reaper.CF_GetCommandText(0, cmd), "^.+:%s(.+)") or reaper.CF_GetCommandText(0, cmd)
 msg = msg:gsub("[.]+$", "")
+end
 end
 local message = initOutputMessage()
 message:initType(string.format("Perform this property to call the %s action.", msg), "Performable")
@@ -33,11 +35,17 @@ message(msg)
 return message
 end,
 set = function(self, action)
+if action == actions.set.perform then
 local message = initOutputMessage()
-if action == nil then
 restorePreviousLayout()
 local oldTracksCount, oldItemsCount = reaper.CountTracks(0), reaper.CountMediaItems(0)
+if type(cmd) == "table" then
+for _, command in ipairs(cmd) do
+reaper.Main_OnCommand(command, 1)
+end
+else
 reaper.Main_OnCommand(cmd, 1)
+end
 local newTracksCount, newItemsCount = reaper.CountTracks(0), reaper.CountMediaItems(0)
 if oldTracksCount < newTracksCount then
 message(string.format("%u tracks added", newTracksCount-oldTracksCount))
