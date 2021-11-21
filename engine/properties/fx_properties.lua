2a7c7483ded34eb696baaa05a91a3aeb9f2d1ba7
Denis A. Shishkin <denis.outsidepro@gmail.com>
2021-11-21 11:11:12 +0300

    *Avoid the ReaScript error when previously set sublayout or property already not exists.

--- engine/properties/fx_properties.lua
+++ engine/properties/fx_properties.lua
@@ -192,6 +192,19 @@ return message
 end
 })
 end
+-- Since the sublayouts and its properties are dynamical at this case, we have to make a little hack with to avoid the call of not existing properties and sublayouts.
+setmetatable(fxLayout[sid].properties, {
+__index = function(self, key)
+fxLayout.pIndex = #fxLayout[sid].properties
+return fxLayout[sid].properties[#fxLayout[sid].properties]
+end
+})
+end
+end
+
+if fxLayout.canProvide() then
+if fxLayout[currentSublayout] == nil then
+currentSublayout = findDefaultSublayout(fxLayout)
 end
 end
 
