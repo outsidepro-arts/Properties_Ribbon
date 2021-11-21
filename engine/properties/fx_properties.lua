f3205d8a23aa4cbb35281da84fdef906beb68290
Denis A. Shishkin <denis.outsidepro@gmail.com>
2021-11-21 16:18:14 +0300

    +Added setting mode

--- engine/properties/fx_properties.lua
+++ engine/properties/fx_properties.lua
@@ -103,24 +103,48 @@ fxLayout:registerSublayout(sid, fxName)
 local fxParmsCount = capi.GetNumParams(i)
 for k = 0, fxParmsCount-1 do
 fxLayout[sid]:registerProperty({
+sm_labels = {
+"Switch to adjusting mode",
+"Set minimal parameter value",
+"Set maximal parameter value",
+"Type raw parameter data",
+"Search for parameter value"
+},
 fxIndex=i,
 parmIndex = tonumber(k),
-get = function(self)
+get = function(self, shouldSaveMode)
 local message = initOutputMessage()
+local mode = layoutExtstate.fxParmMode
+shouldSaveMode = shouldSaveMode or false
+if shouldSaveMode == false then
+if mode and mode > 0 then
+message("Adjusting mode. ")
+layoutExtstate.fxParmMode = nil
+mode = nil
+end
+end
+mode = mode or 0
+if mode > 0 then
+message:initType("Adjust this property to choose needed setting mode for this parameter. Perform this property to activate selected setting.", "Adjustable, performable")
+message(self.sm_labels[mode])
+elseif mode == 0 then
 message:initType("Adjust this property to set necessary value for this parameter.", "Adjustable")
 message(string.format("Parameter %u ", self.parmIndex+1))
 message(({capi.GetParamName(self.fxIndex, self.parmIndex, "")})[2].." ")
 local retval, state = capi.GetFormattedParamValue(self.fxIndex, self.parmIndex, "")
 if retval then
-message(string.format(" value %s", state))
+message(state)
 else
 local retval = capi.GetParamNormalized(self.fxIndex, self.parmIndex)
-message(string.format(" value %d", utils.round(retval, 5)))
+message(tostring(utils.round(retval, 5)))
+end
 end
 return message
 end,
 set = function(self, action)
 local message = initOutputMessage()
+local mode = layoutExtstate.fxParmMode or 0
+if mode == 0 then
 local ajustingValue = config.getinteger("fxParmStep", 0.00001)
 local state, minState, maxState = capi.GetParam(self.fxIndex, self.parmIndex)
 local retvalStep, defStep, _, _, isToggle = capi.GetParameterStepSizes(self.fxIndex, self.parmIndex)
@@ -146,7 +170,7 @@ message("No more next parameter values.")
 end
 else
 if state+ajustingValue <= maxState then
-capi.SetParam(self,fxIndex, self.parmIndex, state+ajustingValue)
+capi.SetParam(self.fxIndex, self.parmIndex, state+ajustingValue)
 capi.EndParamEdit(self.fxIndex, self.parmIndex)
 else
 message("No more next parameter values.")
@@ -182,7 +206,7 @@ message("No more previous parameter values.")
 end
 else
 if state-ajustingValue <= minState then
-capi.SetParam(self,fxIndex, self.parmIndex, state-ajustingValue)
+capi.SetParam(self.fxIndex, self.parmIndex, state-ajustingValue)
 capi.EndParamEdit(self.fxIndex, self.parmIndex)
 else
 message("No more previous parameter values.")
@@ -196,11 +220,90 @@ else
 message("No more previous parameter values.")
 end
 end
-else
-return "This property is adjustable only."
+elseif action == actions.set.toggle then
+message("Setting mode.")
+layoutExtstate.fxParmMode = 1
+message(self:get(true), true)
+return message
 end
 message(self:get())
 return message
+elseif mode > 0 then
+if action == actions.set.increase then
+if (mode+1) <= #self.sm_labels then
+layoutExtstate.fxParmMode = mode+1
+else
+message("No more next parameter settings.")
+end
+elseif action == actions.set.decrease then
+if (mode-1) > 0 then
+layoutExtstate.fxParmMode = mode-1
+else
+message("No more previous parameter settings.")
+end
+elseif action == actions.set.perform then
+if mode == 1 then
+message("Adjusting mode. ")
+layoutExtstate.fxParmMode = 0
+message(self:get())
+return message
+elseif mode == 2 then
+local state, minState, maxState = capi.GetParam(self.fxIndex, self.parmIndex)
+capi.SetParam(self.fxIndex, self.parmIndex, minState)
+capi.EndParamEdit(self.fxIndex, self.parmIndex)
+local retval, curValue = capi.GetFormattedParamValue(self.fxIndex, self.parmIndex, "")
+message(string.format("%s is set", curValue))
+elseif mode == 3 then
+local state, minState, maxState = capi.GetParam(self.fxIndex, self.parmIndex)
+capi.SetParam(self.fxIndex, self.parmIndex, maxState)
+capi.EndParamEdit(self.fxIndex, self.parmIndex)
+local retval, curValue = capi.GetFormattedParamValue(self.fxIndex, self.parmIndex, "")
+message(string.format("%s is set", curValue))
+elseif mode == 4 then
+local state = capi.GetParamNormalized(self.fxIndex, self.parmIndex)
+local retval, answer = reaper.GetUserInputs("Set parameter value", 1, "Type raw parameter value:", tostring(utils.round(state, 5)))
+if retval then
+capi.SetParam(self.fxIndex, self.parmIndex, tonumber(answer))
+capi.EndParamEdit(self.fxIndex, self.parmIndex)
+end
+elseif mode == 5 then
+local retval, curValue = capi.GetFormattedParamValue(self.fxIndex, self.parmIndex, "")
+if retval then
+local retval, answer = reaper.GetUserInputs("Search parameter value", 1, "Type either a part of value string or full string:", curValue)
+if retval then
+local state, minState, maxState = capi.GetParam(self.fxIndex, self.parmIndex)
+local retvalStep, defStep, _, _, isToggle = capi.GetParameterStepSizes(self.fxIndex, self.parmIndex)
+local searchState = minState
+while searchState < maxState do
+searchState = searchState+0.000001
+capi.SetParam(self.fxIndex, self.parmIndex, searchState)
+local wretval, wfxValue = capi.GetFormattedParamValue(self.fxIndex, self.parmIndex)
+if wretval then
+if searcher.simpleSearch(wfxValue, answer) then
+state = searchState
+capi.EndParamEdit(self.fxIndex, self.parmIndex)
+break
+end
+else
+reaper.ShowMessageBox("The parameter values suddenly have lost any string representation.", "Search error", 0)
+break
+end
+end
+if searchState ~= state then
+reaper.ShowMessageBox(string.format("No any parameter value with %s query.", answer), "No results", 0)
+capi.SetParam(self.fxIndex, self.parmIndex, state)
+capi.EndParamEdit(self.fxIndex, self.parmIndex)
+return
+end
+end
+end
+else
+return "This setting is currently unavailable because here's no string  value."
+end
+end
+message(self:get(true))
+return message
+end
 end
 })
 end
