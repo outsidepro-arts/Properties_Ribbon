82d9d557135d1a34f3152029ac944076c18dc926
Denis A. Shishkin <denis.outsidepro@gmail.com>
2022-03-26 16:53:54 +0300

    *Omited the truncation attempt when a track has not any name
    Properties Ribbon requests the track's name using the method which gets a name in any case. Thus, if a track has no any name, this method returns the pattern like "Track X", for example, "Track 25". Before When you had set the smallest truncation length, Properties Ribbon has attempts truncate this string. Now it will be ignored.

--- engine/properties/track_properties.lua
+++ engine/properties/track_properties.lua
@@ -76,10 +76,12 @@ end
 if config.getboolean("reportName", false) == true then
 local retval, name = reaper.GetTrackName(track)
 if retval then
+if name:find("Track") == nil and name:match("%d+") == nil then
 local truncate = config.getinteger("truncateIdBy", 0)
 if truncate > 0 then
 name = utils.truncateSmart(name, truncate)
 end	
+end
 if  name:find("Track") and name:match("%d+") then
 if state == 0 then
 name = name:match("%d+")
