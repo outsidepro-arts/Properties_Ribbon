acb656f5d56a8ecadb89a0c5aace144d6e00cecf
outsidepro-arts <denis.outsidepro@gmail.com>
2024-01-24 02:16:39 +0300

    Rewritten whole script due my fault
    I thought that REAPER stores the B64 of the filename only but not file path.

--- Properties Ribbon - Track, master track, item or envelope properties depending on focus.lua
+++ Properties Ribbon - Track, master track, item or envelope properties depending on focus.lua
@@ -5,35 +5,9 @@ License: MIT License
 ]]
 --
 
--- REAPER hack to prevent useless undo points creation
-reaper.defer(function() end)
+package.path = select(2, reaper.get_action_context()):match('^.+[\\//]') .. "?//init.lua"
 
-contextualActions = setmetatable({
-	[0] = {
-		"_RS281e717ec90e00117f1fabe94e66cab67483b46a",
-		"_RS4c1bcc02859b102d0c590b483d8a454ffb12bcd1"
-	},
-	[1] = {
-		"_RS494478b1fa52f40e1d43ada25d538334f069b0d0"
-	},
-	[2] = {
-		"_RSf75f25d3f38202cfab9a2ebc54a87c7bb04c1916"
-	}
-}, {
-	__index = function(self)
-		return self[0]
-	end
-})
+require "properties_ribbon"
 
-definedAction = contextualActions[0][(reaper.GetMasterTrackVisibility() & 1) == 1 and 2 or 1]
 
-if reaper.GetCursorContext() == 2 or reaper.CountTracks(0) > 0 then
-	definedAction = contextualActions[reaper.GetCursorContext()]
-	[(reaper.GetCursorContext() == 0 and reaper.GetLastTouchedTrack() == reaper.GetMasterTrack(0)) and 2 or 1]
-else
-	reaper.SetOnlyTrackSelected(reaper.GetMasterTrack(0), true)
-	reaper.SetCursorContext(0)
-end
-
-reaper.Main_OnCommand(
-	reaper.NamedCommandLookup(definedAction), 0)
+dofile(PropertiesRibbon.proposeLayout(true))
