517c24f857e5289c377c576b3da0006ada35f387
outsidepro-arts <denis.outsidepro@gmail.com>
2024-09-14 15:52:22 +0300

    composeThreePositionProperty: added optional parameter checkAbilityFunc
    Allows to pass a function which should check the ability to do any changes for this object. Before the three-position property will attempt to do any changes, it will call this function and will pass either current object in a objects array or this object when it is not array. If the function returns true, the three-position property will change this object. If this function returns false, the property will no changes.
    Syntax:
    ```Lua
    ---@param obj any
    ---@return boolean
    function checkAbilityFunction(objItem)
    ```

--- properties_ribbon/macros/tools.lua
+++ properties_ribbon/macros/tools.lua
@@ -1,6 +1,6 @@
 ---composes the three-position setter property
 ---@param obj any
----@param minRootMax any
+---@param minRootMax table
 ---|"'min'" # the value which should be set when user decreases the setter
 ---"'root'" # the value which should be set when user performs the setter
 ---|"'max'" # the value which should be set when user increases the setter
@@ -8,14 +8,26 @@
 ---"true" # the message when obj is a table
 ---"false" # the message when obj is not a table
 ---@param setValueFunc function
+---@param checkAbilityFunc function
 ---@return table
-function composeThreePositionProperty(obj, minRootMax, setMessages, setValueFunc)
+function composeThreePositionProperty(obj, minRootMax, setMessages, setValueFunc, checkAbilityFunc)
+	checkAbilityFunc = checkAbilityFunc or function()
+		return true
+	end
 	local t = {
 		get = function(self, parent)
 			local message = initOutputMessage()
 			message:initType(
 				"Adjust and perform this three-state setter to set the needed value specified in parentheses.")
 			message("three-position setter")
+			if not istable(obj) then
+				if not checkAbilityFunc(obj) then
+					message:addType(
+						" This property is unavailable now because this object reports that it cannot be changed.", 1)
+					message:changeType("Unavailable", 2)
+					return message
+				end
+			end
 			message(string.format(" (%s - %s, ", actions.set.decrease.label, minRootMax.representation[minRootMax.min]))
 			message(string.format("%s - %s, ", actions.set.perform.label, minRootMax.representation[minRootMax.rootmean]))
 			message(string.format("%s - %s)", actions.set.increase.label, minRootMax.representation[minRootMax.max]))
@@ -30,10 +42,16 @@ function composeThreePositionProperty(obj, minRootMax, setMessages, setValueFunc
 			message(string.format(setMessages[istable(obj)], minRootMax.representation[vls[direction]]))
 			if istable(obj) then
 				for _, o in ipairs(obj) do
-					setValueFunc(o, vls[direction])
+					if checkAbilityFunc(o) then
+						setValueFunc(o, vls[direction])
+					end
 				end
 			else
-				setValueFunc(obj, vls[direction])
+				if checkAbilityFunc(obj) then
+					setValueFunc(obj, vls[direction])
+				else
+					return false, "This property is unavailable for this object"
+				end
 			end
 			return true, message
 		end,
@@ -43,15 +61,19 @@ function composeThreePositionProperty(obj, minRootMax, setMessages, setValueFunc
 			message(string.format(setMessages[istable(obj)], minRootMax.representation[state]))
 			if istable(obj) then
 				for _, o in ipairs(obj) do
-					setValueFunc(o, state)
+					if checkAbilityFunc(o) then
+						setValueFunc(o, state)
+					end
 				end
 			else
-				setValueFunc(obj, state)
+				if checkAbilityFunc(obj) then
+					setValueFunc(obj, state)
+				else
+					return false, "This property is unavailable for this object"
+				end
 			end
 			return true, message
 		end
 	}
 	return t
 end
-
-
